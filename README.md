4def7d09b57c730c7dcb68579c6a855aeeda4c5b
ydaigo <b1614808@planet.kanazawa-it.ac.jp>
2023-03-09 21:04:37 +0900

    fix

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
-<style type="text/css">
-.iframe-wrapper iframe {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-}
-</style>
-<div class="iframe-wrapper">
-  <iframe src="http://127.0.0.1:7860"></iframe>
-</div>
+# chatgpt-name-craft
+
+## 準備
+
+.envファイルを作成
+
+```.env
+OPENAI_API_KEY=[APIキー]
+```
+
+## 通常の実行方法
+
+```
+pip install -r requirements.txt
+```
+
+```
+python main.py
+```
+
+## Dockerでの実行方法
+
+```
+docker build . -t chatgpt-name-craft:0.0.1
+```
+
+```
+docker run -p 7860:7860 chatgpt-name-craft:0.0.1
+```
--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BASE_PROMPT = '''
-あなたはプログラマです。 {}で【{}】の{}を考えてください。10文字以内で端的に答えて。
+{}で【{}】の{}を考えてください。50文字以内で答えて。
 '''
 
 
@@ -20,18 +20,23 @@ def create_name(programming_language, target, identifier):
                                                                   identifier),
                                         temperature=0,
                                         max_tokens=7)
-    return response.choices[0].text
+    return response.choices[0].text.replace("\n", "")
 
 
-inputs = [gr.Radio(label="プログラム言語を選択してください。",
-                   choices=["Python", "JavaScript", "Java", "Golang", "PHP", "Ruby", "C言語", "C++", "C#"],
-                   value="Python"),
-          gr.Text(label="日本語で命名してほしい内容を書いてください。", value="商品番号"),
-          gr.Radio(label="識別子の種類を選択してください。", choices=["変数名", "関数名", "クラス名", "定数名"],
-                   value="変数名"),
-          ]
-demo = gr.Interface(fn=create_name, inputs=inputs, outputs=gr.Text(label="命名結果"), allow_flagging='never',
-                    title="プログラマーのための命名工房", description="ChatGPTを利用して変数名や関数名を命名します。")
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            inputs = [gr.Radio(label="プログラム言語を選択してください。",
+                               choices=["Python", "JavaScript", "Java", "Go言語", "PHP", "Ruby", "C言語", "C++", "C#"],
+                               value="Python"),
+                      gr.Text(label="日本語で命名してほしい内容を書いてください。", value="商品番号"),
+                      gr.Radio(label="識別子の種類を選択してください。", choices=["変数名", "関数名", "クラス名", "定数名"],
+                               value="変数名"),
+                      ]
+        with gr.Column():
+            btn = gr.Button("送信")
+            outputs = gr.Text(label="命名結果")
+    btn.click(fn=create_name, inputs=inputs, outputs=outputs)
 
 if __name__ == "__main__":
-    demo.launch(show_api=False)
+    demo.launch(show_api=False, server_name="0.0.0.0")
